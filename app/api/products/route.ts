19344e60ff3baf740888dbd804e7248643a5435b
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-03-11 21:33:42 +0500

    Updated Product schmea to use default uuid + Implemented APIs for create and get Products

---
+++ app/api/products/route.ts
@@ -0,0 +1,59 @@
+import db from "@/db/drizzle";
+import { products } from "@/db/schema";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const allProducts = await db.select().from(products);
+
+        return NextResponse.json(
+            {
+                success: true,
+                message: "All Products Fetched",
+                data: allProducts,
+            },
+            { status: 200 },
+        );
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Internal Server Error",
+            },
+            { status: 500 },
+        );
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+
+        const insertedProduct = await db.insert(products).values({
+            name: body.name,
+            description: body.description,
+            price: body.price,
+            imageUrl: body.imageUrl,
+            quantity: body.quantity,
+        });
+
+        return NextResponse.json(
+            {
+                success: true,
+                message: "Product Created",
+                data: insertedProduct,
+            },
+            { status: 201 },
+        );
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Internal Server Error",
+            },
+            { status: 500 },
+        );
+    }
+}
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,13 @@
-import { pgEnum, pgTable, text, real, check } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
+import {
+    pgEnum,
+    pgTable,
+    text,
+    integer,
+    check,
+    real,
+    uuid,
+} from "drizzle-orm/pg-core";
 
 export const userRoleEnum = pgEnum("user_role", ["Customer", "Admin"]);
 
@@ -13,13 +21,17 @@ export const users = pgTable("users", {
 export const products = pgTable(
     "products",
     {
-        id: text("id").primaryKey(),
+        id: uuid().primaryKey().defaultRandom(),
         name: text("name").notNull(),
         description: text("description"),
         price: real("price").notNull(),
         imageUrl: text("imageUrl").notNull(),
+        quantity: integer("quantity").notNull(),
     },
-    (table) => [check("price_gte_0", sql`${table.price} >= 0`)],
+    (table) => [
+        check("price_gte_0", sql`${table.price} >= 0`),
+        check("quantity_gte_0", sql`${table.quantity} >= 0`),
+    ],
 );
 
 export type UserRole = (typeof userRoleEnum.enumValues)[number];
